bb2eb8473a6740d6f80d53a6c5616d729ad3b83e
devbelek <belekasrarov10@gmail.com>
2023-08-28 00:21:11 +0600

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-sc9-db*cgvs%*7-nj0=by4d47abi0nrq02oz=ed))@#bo=_3r%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
